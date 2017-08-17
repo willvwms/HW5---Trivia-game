9bcc422b352f822dc82b5103427dbf30f6505f98
Will Williams <will.wms@gmail.com>
2017-08-17 18:24:09 -0500

    .

---
+++ objects.js
@@ -0,0 +1,161 @@
+var gameData = { 
+	list: [
+
+	{
+	"questionText" : "What is the name of Jon Snow's wolf?",
+	"a" : "Winter",
+	"b" : "Ghost",
+	"c" : "Ice",
+	"d" : "Milk",
+	"correctLetter" : "b",
+	"correctAnswer" : "Ghost",
+	},
+
+	{
+	"questionText" : "What are the words of House Lannister?",
+	"a":"A Lanniser Always Pays His Debts",
+	"b":"Revenge, Honor, Power",
+	"c":"A Lion Never Concerns Himself with the Opinions of the Sheep",
+	"d":"Hear me roar",
+	"correctLetter" : "d",
+	"correctAnswer" : "Hear mMe Roar",
+	},
+
+	{
+	"questionText" : "What are the words of House Tyrell?",
+	"a":"Growing Strong",
+	"b":"Peace and Prosperity",
+	"c":"Bounty, Beauty, Honor",
+	"d":"Ever Summer",
+	"correctLetter" : "a",
+	"correctAnswer" : "Growing Strong",
+	},
+
+	{
+	"questionText" : "What are the words of House Martell?",
+	"a":"In Sand, Sweetness",
+	"b":"Children of the Sun",
+	"c":"Blood and Vengeance",
+	"d":"Unbowed, Unbent, Unbroken",
+	"correctLetter" : "d",
+	"correctAnswer" : "Unbowed, Unbent, Unbroken",
+	},
+
+	{
+	"questionText" : "What are the words of House Targaryen?",
+	"a":"Fire and Blood",
+	"b":"In Fire, Strength",
+	"c":"Justice and Mercy",
+	"d":"Summer is Coming",
+	"correctLetter" : "a",
+	"correctAnswer" : "Fire and Blood",
+	},
+
+	{
+	"questionText" : "What is the sigil of House Martell?",
+	"a":"Sun and Spear",
+	"b":"Winged Ship",
+	"c":"Desert Viper",
+	"d":"The Broken Tower",
+	"correctLetter" : "a",
+	"correctAnswer" : "Sun and Spear",
+	},
+
+	{
+	"questionText" : "Sam finds out that the baby Gilly is breastfeeding is really ___'s child.",
+	"a":"Lord Commander Mormont",
+	"b":"Craster",
+	"c":"Ned Stark",
+	"d":"The Night King",
+	"correctLetter" : "b",
+	"correctAnswer" : "Craster",
+	},
+
+	{
+	"questionText" : "The wives of dead Khals are known as ____.",
+	"a":"Dosh Khaleen",
+	"b":"The Motherless",
+	"c":"Valar Morghulis",
+	"d":"The Forgotten",
+	"correctLetter" : "a",
+	"correctAnswer" : "Dosh Khaleen",
+	},
+
+	{
+	"questionText" : "_____ conspired with ______ to kill Joffrey at the Purple Wedding. ",
+	"a":"Sansa, Tyrion",
+	"b":"Littlefinger, Catelyn",
+	"c":"Olenna, Littlefinger",
+	"d":"Sansa, The Hound",
+	"correctLetter" : "c",
+	"correctAnswer" : "Olenna, Littlefinger",
+	},
+
+	{
+	"questionText" : "What is the meaning of Valar Dohaeris?",
+	"a":"All men must die",
+	"b":"All men must serve",
+	"c":"Where are my dragons",
+	"d":"Take what is yours",
+	"correctLetter" : "b",
+	"correctAnswer" : "All men must serve",
+	},
+
+	{
+	"questionText" : "Who was the mastermind behind the Red Wedding?",
+	"a":"Walder Frey",
+	"b":"Roose Bolton",
+	"c":"Littlefinger",
+	"d":"Tywin Lannister",
+	"correctLetter" : "d",
+	"correctAnswer" : "Tywin Lannister",
+	},
+
+	{
+	"questionText" : "Where is Skroth spoken, and by whom?",
+	"a":"Qarth, the Qartheen",
+	"b":"The Lands of Always Winter, the Others",
+	"c":"Shadow Lands, the Shadowbinders",
+	"d":"Old Empire of Ghis, the Ghiscari",
+	"correctLetter" : "b",
+	"correctAnswer" : "The Lands of Always Winter, the Others",
+	},
+
+	{
+	"questionText" : "Who was behind the attempt to kill Bran in his sleep?",
+	"a":"Tyrion Lannister",
+	"b":"Joffrey Baratheon",
+	"c":"Tywin Lannister",
+	"d":"Petyr Baelish",
+	"correctLetter" : "b",
+	"correctAnswer" : "Joffrey Baratheon",
+	},
+
+	{
+	"questionText" : "What is going to be the name of the sixth book of the series?",
+	"a":"A Song of Spring",
+	"b":"The Children of Summer",
+	"c":"The Winds of Winter",
+	"d":"The End of Autumn",
+	"correctLetter" : "c",
+	"correctAnswer" : "The Winds of Winter",
+	},
+	
+	{
+	"questionText" : "Which of the following is NOT a title held by Daenerys Targaryen?",
+	"a":"Khaleesi of the Great Grass Sea",
+	"b":"Lady Paramount of Essos",
+	"c":"Lady Regnant of the Seven Kingdoms",
+	"d":"Protector of the Realm",
+	"correctLetter" : "b",
+	"correctAnswer" : "Lady Paramount of Essos",
+	}
+]
+
+}; /// end of entire questionsAndAnswers object definition 
+
+// Picture array object
+var answerGif = {
+	right:["images/right.png"],
+	wrong:["images/wrong.png"]
+};
